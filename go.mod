4cca39872999ccf9b7d136337c9ed4ba59d971ff
hippodribble <gdreynolds@gmail.com>
2022-02-20 13:00:52 +0200

    New commit

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module dummy
 
 go 1.17
 
-require (
-	github.com/hippodribble/quickcheck v0.1.2
-	github.com/hippodribble/testignore v0.1.7
-)
+require github.com/hippodribble/alpha v0.0.3
--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import(
-	g "github.com/hippodribble/testignore/geometry"
-	K "github.com/hippodribble/alpha/utils"
+	
+	g "github.com/hippodribble/alpha/utils/geometry"
+	gps "github.com/hippodribble/alpha/utils/gps"
 )
 
 func main(){
-	println(K.C)
+	
 	p:=g.Point{3,4,"Smith"}
 	println(p.Label)
+
+	conn:=gps.GPSConnection{":5444",57600}
+	println(conn.Port)
 }
